5c421a52fc7e175749b3180e1f44d7608e6824e1
sv raman <svraman@corenttech.com>
2024-04-22 11:40:12 +0530

    0.2.0 Added pandas.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import random
+import pandas as pd
 
 st.set_page_config(
         page_title="SVRaman Streamlit Test",
@@ -6,3 +8,34 @@ st.set_page_config(
 
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 st.write("Hello world4")
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+
+df = pd.DataFrame(
+    {
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+    }
+)
+st.dataframe(
+    df,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
+)
